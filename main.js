e5bdde78fe5ff6bdc847fdfa8068ade0e4f2f8dc
fujunchun <394744352@qq.com>
2017-03-08 16:45:26 +0800

    Update main.js
    文件名中包含_zh结尾的替换为_fanti，可在
    let simple_tag = /_zh/;
    let traditional_tag = "_fanti";
    处修改设置

--- main.js
+++ main.js
@@ -8,9 +8,15 @@ let S = require("simplebig");
 
 let src = "src";//源目录
 let dist = "dist";//生成的目录, 只支持一级目录
+/**
+ * 语言标识，如果文件有_zh结尾，则被替换为繁体标识
+ * @type {String}
+ */
+let simple_tag = /_zh/;
+let traditional_tag = "_fanti";
 
 if (fs.existsSync(dist)) {
-    deleteFolder(dist);
+    deleteDirectory(dist);
 }
 
 fs.mkdirSync(dist);
@@ -31,8 +37,15 @@ glob(path.join(src, "**"), {"nodir": true}, (err, files) => {
                 if(err){
                     return console.log(err);
                 }
+                
+                let target = file.split("/");
+                let last = target.length - 1;
 
-                let target = file.replace(src, dist);
+                target[0] = dist;
+                target[last] = target[last].replace(simple_tag, traditional_tag);
+
+                target = target.join("/");
+
                 let fanti = S.s2t(data.toString());
 
                 fs.writeFile(target, fanti, (err) => {
@@ -69,15 +82,15 @@ function createDistDir(files) {
 }
 
 //删除文件夹
-function deleteFolder(directory) {
+function deleteDirectory(directory) {
     let files = [];
     if (fs.existsSync(directory)) {
         files = fs.readdirSync(directory);
         files.forEach(function (file, index) {
             var curPath = path.join(directory, file);
-            if (fs.statSync(curPath).isDirectory()) { // recurse
-                deleteFolder(curPath);
-            } else { // delete file
+            if (fs.statSync(curPath).isDirectory()) {
+                deleteDirectory(curPath);
+            } else { 
                 fs.unlinkSync(curPath);
             }
         });
